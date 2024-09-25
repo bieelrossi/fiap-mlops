2ed7380e5c90037fd4a4b161483010b6fce68d36
bieelrossi <gabrielc.rossi@outlook.com>
2024-09-24 23:58:12 -0300

    inclusao de modelo de inadimplencia

--- servingmodel.py
+++ servingmodel.py
@@ -5,57 +5,28 @@ import numpy as np
 from flask import Flask, jsonify, request
 import sys
 
-class NpEncoder(json.JSONEncoder):
-    def default(self, obj):
-        if isinstance(obj, np.integer):
-            return int(obj)
-        elif isinstance(obj, np.floating):
-            return float(obj)
-        elif isinstance(obj, np.ndarray):
-            return obj.tolist()
-        else:
-            return super(NpEncoder, self).default(obj)
-
 app = Flask(__name__)
-app.json_encoder = NpEncoder
-modelo = None
 
-@app.route("/", methods=['GET', 'POST'])
-def call_home(request = request):
-    print(request.values)
-    return "SERVER IS RUNNING! \n" + json.dumps({
-        "args": str(sys.argv),
-    })
+modelo = None
 
 @app.route("/predict", methods=['GET', 'POST'])
 def call_predict(request = request):
     print(request.values)
 
     json_ = request.json
-    campos = pd.DataFrame(json_)
-
+    campos = pd.DataFrame([json_])
+    
     if campos.shape[0] == 0:
         return "Dados de chamada da API estão incorretos.", 400
 
-    for col in modelo.independentcols:
-        if col not in campos.columns:
-            campos[col] = 0
-    x = campos[modelo.independentcols]
+    prediction = modelo.predict(campos)
 
-    prediction = modelo.predict(x)
-
-
-    ret = {'prediction': list(prediction)}
-    if hasattr(modelo, 'predict_proba'):
-        predict_proba = modelo.predict_proba(x)
-        ret['proba'] = list(predict_proba)
-
-    return app.response_class(response=json.dumps(ret, cls=NpEncoder), mimetype='application/json')
+    return jsonify(str([1 if prob > 0.5 else 0 for prob in prediction]))
 
 if __name__ == '__main__':
     args = sys.argv[1:]
     if len(args) < 1:
-        args.append('models/modelo01.joblib')
+        args.append('models/inadimplencia_lgbm.joblib')
     if len(args) < 2:
         args.append('8080')
 
@@ -63,6 +34,6 @@ if __name__ == '__main__':
 
     modelo = joblib.load(args[0])
     # app.run(port=8080, host='0.0.0.0')
-    app.run(port=args[1], host='0.0.0.0')
+    app.run(port=args[1], host='0.0.0.0', debug=True)
     pass
 
